a808bd82ffda38235e3eb0dec04e3dd1f3e03fed
Tyler C Field <tfield@scholar-fe04.rcac.purdue.edu>
2020-02-05 13:51:52 -0500

    initial commit, except dir_info.sh

---
+++ Lab04/tfield_my_backup.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+# Backs up all files in the ABE65100 folder to the ABE65100_backup folder
+# Tyler Field 2/5/2020
+
+cp -r ~/ABE65100 ~/ABE65100-backup
+
+echo Backup of all ABE65100 files is completed
